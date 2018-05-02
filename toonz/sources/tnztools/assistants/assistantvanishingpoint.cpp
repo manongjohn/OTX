b07f9810d54e4abf544a0c8e8e0b47b34d932836
Ivan Mahonin <bh@icystar.com>
2018-05-02 17:22:37 +0700

    #assistants: TAssistantVanishingPoint

---
+++ toonz/sources/tnztools/assistants/assistantvanishingpoint.cpp
@@ -0,0 +1,41 @@
+
+
+#include "assistantvanishingpoint.h"
+#include "guidelineline.h"
+
+// TnzCore includes
+#include "tgl.h"
+
+
+//*****************************************************************************************
+//    TAssistantVanishingPoint implementation
+//*****************************************************************************************
+
+TAssistantVanishingPoint::TAssistantVanishingPoint(TMetaObject &object):
+  TAssistant(object)
+{
+  m_points.push_back(TAssistantPoint(
+    TAssistantPoint::CircleCross ));
+}
+
+void
+TAssistantVanishingPoint::getGuidelines(
+  const TPointD &position,
+  const TAffine &toTool,
+  TGuidelineList &outGuidelines ) const
+{
+  outGuidelines.push_back(TGuidelineP(
+    new TGuidelineInfiniteLine(
+      toTool*m_points.front().position,
+      position )));
+}
+
+void
+TAssistantVanishingPoint::draw(TToolViewer *viewer) const {
+  double pixelSize = sqrt(tglGetPixelSize2());
+  const TPointD &p = m_points.front().position;
+  TPointD dx(20.0*pixelSize, 0.0);
+  TPointD dy(0.0, 10.0*pixelSize);
+  drawSegment(p-dx-dy, p+dx+dy, pixelSize);
+  drawSegment(p-dx+dy, p+dx-dy, pixelSize);
+}
---
+++ toonz/sources/tnztools/assistants/guidelineline.cpp
@@ -0,0 +1,117 @@
+
+
+#include "guidelineline.h"
+
+// TnzCore includes
+#include "tgl.h"
+
+
+//*****************************************************************************************
+//    TGuidelineLineBase implementation
+//*****************************************************************************************
+
+TGuidelineLineBase::TGuidelineLineBase(const TPointD &p0, const TPointD &p1):
+  p0(p0), p1(p1) { }
+
+TPointD
+TGuidelineLineBase::calcDirection(const TPointD &p0, const TPointD &p1) {
+  TPointD d = p1 - p0;
+  double k = norm2(d);
+  return k > TConsts::epsilon ? d*(1.0/k) : TPointD();
+}
+
+void
+TGuidelineLineBase::truncateInfiniteLine(const TRectD &bounds, TPointD &p0, TPointD &p1) {
+  TPointD d = p0 - p1;
+  TDimensionD size = bounds.getSize();
+  if (fabs(d.x)*bounds.y0 > bounds.x0*fabs(d.y)) {
+    // horizontal
+    if (fabs(d.x) < TConsts::epsilon) return;
+    double k = d.y/d.x;
+    p1 = TPointD(bounds.x1, p0.y + k*(bounds.x1 - p0.x));
+    p0 = TPointD(bounds.x0, p0.y + k*(bounds.x0 - p0.x));
+  } else {
+    // vertical
+    if (fabs(d.y) < TConsts::epsilon) return;
+    double k = d.x/d.y;
+    p1 = TPointD(p0.x + k*(bounds.y1 - p0.y), bounds.y1);
+    p0 = TPointD(p0.x + k*(bounds.y0 - p0.y), bounds.y0);
+  }
+}
+
+void
+TGuidelineLineBase::drawInliniteLine(const TPointD &p0, const TPointD &p1, bool ray, bool active) const {
+  TAffine4 modelview;
+  glGetDoublev(GL_MODELVIEW_MATRIX, modelview.a);
+
+  TAffine matrix = modelview.get2d();
+  TPointD pp0 = matrix*p0;
+  TPointD pp1 = matrix*p1;
+  truncateInfiniteLine(TRectD(-1.0, -1.0, 1.0, 1.0), pp0, pp1);
+
+  TAffine matrixInv = matrix.inv();
+  double pixelSize = norm(TPointD(matrixInv.a11 + matrixInv.a12, matrixInv.a21 + matrixInv.a22));
+  drawSegment((ray ? p0 : matrixInv*pp0), matrixInv*pp1, pixelSize, active);
+}
+
+
+//*****************************************************************************************
+//    TGuidelineLine implementation
+//*****************************************************************************************
+
+TGuidelineLine::TGuidelineLine(const TPointD &p0, const TPointD &p1):
+  TGuidelineLineBase(p0, p1),
+  dir(calcDirection(p0, p1)),
+  dist(norm(p1 - p0)) { }
+
+TTrackPoint
+TGuidelineLine::transformPoint(const TTrackPoint &point) const {
+  TTrackPoint p(point);
+  p.position = p0 + dir * std::max(0.0, std::min(dist, ((p.position - p0)*dir)));
+  return p;
+}
+
+void
+TGuidelineLine::draw(bool active) const
+  { drawSegment(p0, p1, sqrt(tglGetPixelSize2()), active); }
+
+
+//*****************************************************************************************
+//    TGuidelineInfiniteLine implementation
+//*****************************************************************************************
+
+TGuidelineInfiniteLine::TGuidelineInfiniteLine(const TPointD &p0, const TPointD &p1):
+  TGuidelineLineBase(p0, p1),
+  dir(calcDirection(p0, p1)) { }
+
+TTrackPoint
+TGuidelineInfiniteLine::transformPoint(const TTrackPoint &point) const {
+  TTrackPoint p(point);
+  p.position = p0 + dir * ((p.position - p0)*dir);
+  return p;
+}
+
+void
+TGuidelineInfiniteLine::draw(bool active) const
+  { drawInliniteLine(p0, p1, false, active); }
+
+
+//*****************************************************************************************
+//    TGuidelineRay implementation
+//*****************************************************************************************
+
+TGuidelineRay::TGuidelineRay(const TPointD &p0, const TPointD &p1):
+  TGuidelineLineBase(p0, p1),
+  dir(calcDirection(p0, p1)) { }
+
+TTrackPoint
+TGuidelineRay::transformPoint(const TTrackPoint &point) const {
+  TTrackPoint p(point);
+  p.position = p0 + dir * std::max(0.0, ((p.position - p0)*dir));
+  return p;
+}
+
+void
+TGuidelineRay::draw(bool active) const
+  { drawInliniteLine(p0, p1, true, active); }
+
