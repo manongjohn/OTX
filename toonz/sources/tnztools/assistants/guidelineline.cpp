6327546c084e9c3438868a7390c392953b87a626
Ivan Mahonin <bh@icystar.com>
2018-05-20 00:01:44 +0700

    #assistants: perspective grid for vanishing point

--- toonz/sources/tnztools/assistants/guidelineline.cpp
+++ toonz/sources/tnztools/assistants/guidelineline.cpp
@@ -22,7 +22,7 @@ TGuidelineLineBase::calcDirection(const TPointD &p0, const TPointD &p1) {
 
 
 static bool
-truncateX(double &x0, double &y0, double &x1, double &y1, double minx, double maxx) {
+fitX(double &x0, double &y0, double &x1, double &y1, double minx, double maxx) {
   double dx = x1 - x0;
   if (fabs(dx) < TConsts::epsilon) return false;
   double k = (y1 - y0)/dx;
@@ -36,21 +36,36 @@ truncateX(double &x0, double &y0, double &x1, double &y1, double minx, double ma
   return true;
 }
 
+static bool
+truncateX(double &x0, double &y0, double &x1, double &y1, double minx, double maxx) {
+  if (x0 <= minx && x1 <= minx) return false;
+  if (x0 >= maxx && x1 >= maxx) return false;
+  double dx = x1 - x0;
+  if (fabs(dx) < TConsts::epsilon) return true;
+  double k = (y1 - y0)/dx;
+  if (dx > 0.0) {
+    if (x0 < minx) { y0 += k*(minx - x0); x0 = minx; }
+    if (x1 > maxx) { y1 += k*(maxx - x1); x1 = maxx; }
+  } else {
+    if (x0 > maxx) { y0 += k*(maxx - x0); x0 = maxx; }
+    if (x1 < minx) { y1 += k*(minx - x1); x1 = minx; }
+  }
+  return true;
+}
+
 bool
 TGuidelineLineBase::truncateInfiniteLine(const TRectD &bounds, TPointD &p0, TPointD &p1) {
   if (bounds.isEmpty()) return false;
   TPointD d = p0 - p1;
   TDimensionD size = bounds.getSize();
-  if (fabs(d.x)*bounds.y0 > bounds.x0*fabs(d.y)) {
+  if (fabs(d.x)*(bounds.y1 - bounds.y0) > fabs(d.y)*(bounds.x1 - bounds.x0)) {
     // horizontal
-    if (!truncateX(p0.x, p0.y, p1.x, p1.y, bounds.x0, bounds.x1)) return false;
-    if (p0.y <= bounds.y0 && p1.y <= bounds.y0) return false;
-    if (p0.y >= bounds.y1 && p1.y >= bounds.y1) return false;
+    if (!fitX     (p0.x, p0.y, p1.x, p1.y, bounds.x0, bounds.x1)) return false;
+    if (!truncateX(p0.y, p0.x, p1.y, p1.x, bounds.y0, bounds.y1)) return false;
   } else {
     // vertical
-    if (!truncateX(p0.y, p0.x, p1.y, p1.x, bounds.y0, bounds.y1)) return false;
-    if (p0.x <= bounds.x0 && p1.x <= bounds.x0) return false;
-    if (p0.x >= bounds.x1 && p1.x >= bounds.x1) return false;
+    if (!fitX     (p0.y, p0.x, p1.y, p1.x, bounds.y0, bounds.y1)) return false;
+    if (!truncateX(p0.x, p0.y, p1.x, p1.y, bounds.x0, bounds.x1)) return false;
   }
   return true;
 }
@@ -59,10 +74,11 @@ bool
 TGuidelineLineBase::truncateInfiniteRay(const TRectD &bounds, TPointD &p0, TPointD &p1) {
   if (bounds.isEmpty()) return false;
   TRectD b(bounds);
-  if (b.contains(p0)) {
-    (p0.x < p1.x ? b.x0 : b.x1) = p0.x;
-    (p0.y < p1.y ? b.y0 : b.y1) = p0.y;
-  }
+  if (p0.x <= p1.x && b.x0 <= p0.x) b.x0 = p0.x;
+  if (p0.x >  p1.x && b.x1 >  p0.x) b.x1 = p0.x;
+  if (p0.y <= p1.y && b.y0 <= p0.y) b.y0 = p0.y;
+  if (p0.y >  p1.y && b.y1 >  p0.y) b.y1 = p0.y;
+  if (b.isEmpty()) return false;
   return truncateInfiniteLine(b, p0, p1);
 }
 
