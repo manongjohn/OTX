c050a3f251ee09a1daf18afda30a1e1ff9e64bf0
Ivan Mahonin <bh@icystar.com>
2018-05-31 19:37:14 +0700

    #assistants: assistants for BrushTool

--- toonz/sources/tnztools/toonzrasterbrushtool.cpp
+++ toonz/sources/tnztools/toonzrasterbrushtool.cpp
@@ -53,6 +53,7 @@ TEnv::DoubleVar BrushSmooth("InknpaintBrushSmooth", 0);
 TEnv::IntVar BrushDrawOrder("InknpaintBrushDrawOrder", 0);
 TEnv::IntVar RasterBrushPencilMode("InknpaintRasterBrushPencilMode", 0);
 TEnv::IntVar BrushPressureSensitivity("InknpaintBrushPressureSensitivity", 1);
+TEnv::IntVar BrushAssistants("InknpaintBrushAssistants", 0);
 TEnv::DoubleVar RasterBrushHardness("RasterBrushHardness", 100);
 TEnv::DoubleVar RasterBrushModifierSize("RasterBrushModifierSize", 0);
 
@@ -827,16 +828,21 @@ ToonzRasterBrushTool::ToonzRasterBrushTool(std::string name, int targetType)
     , m_drawOrder("Draw Order:")
     , m_pencil("Pencil", false)
     , m_pressure("Pressure", true)
+    , m_assistants("Assistants", false)
     , m_modifierSize("ModifierSize", -3, 3, 0, true)
-    , m_rasterTrack(0)
-    , m_styleId(0)
-    , m_bluredBrush(0)
+    , m_rasterTrack()
+    , m_tileSet()
+    , m_tileSaver()
+    , m_styleId()
+    , m_minThick()
+    , m_maxThick()
+    , m_targetType(targetType)
+    , m_bluredBrush()
     , m_active(false)
     , m_enabled(false)
     , m_isPrompting(false)
     , m_firstTime(true)
     , m_presetsLoaded(false)
-    , m_targetType(targetType)
     , m_workingFrameId(TFrameId())
     , m_notifier(0) {
   bind(targetType);
@@ -855,6 +861,7 @@ ToonzRasterBrushTool::ToonzRasterBrushTool(std::string name, int targetType)
   m_drawOrder.setId("DrawOrder");
 
   m_prop[0].bind(m_pressure);
+  m_prop[0].bind(m_assistants);
 
   m_prop[0].bind(m_preset);
   m_preset.setId("BrushPreset");
@@ -1031,8 +1038,14 @@ void ToonzRasterBrushTool::updateTranslation() {
   m_preset.setQStringName(tr("Preset:"));
   m_preset.setItemUIName(CUSTOM_WSTR, tr("<custom>"));
   m_pencil.setQStringName(tr("Pencil"));
+  m_assistants.setQStringName(tr("Assistants"));
   m_pressure.setQStringName(tr("Pressure"));
 }
+
+//---------------------------------------------------------------------------------------------------
+
+bool ToonzRasterBrushTool::isAssistantsEnabled() const
+  { return m_assistants.getValue(); }
 
 //---------------------------------------------------------------------------------------------------
 
@@ -1112,6 +1125,7 @@ void ToonzRasterBrushTool::onActivate() {
     m_hardness.setValue(RasterBrushHardness);
 
     m_pressure.setValue(BrushPressureSensitivity ? 1 : 0);
+    m_assistants.setValue(BrushAssistants ? 1 : 0);
     m_firstTime = false;
     m_smooth.setValue(BrushSmooth);
     m_modifierSize.setValue(RasterBrushModifierSize);
@@ -1928,10 +1942,13 @@ bool ToonzRasterBrushTool::onPropertyChanged(std::string propertyName) {
     RasterBrushPencilMode = m_pencil.getValue();
   } else if (propertyName == m_pressure.getName()) {
     BrushPressureSensitivity = m_pressure.getValue();
-  } else if (propertyName == m_hardness.getName())
+  } else if (propertyName == m_assistants.getName()) {
+    BrushAssistants = m_assistants.getValue();
+  } else if (propertyName == m_hardness.getName()) {
     setWorkAndBackupImages();
-  else if (propertyName == m_modifierSize.getName())
+  } else if (propertyName == m_modifierSize.getName()) {
     RasterBrushModifierSize = m_modifierSize.getValue();
+  }
 
   if (propertyName == m_hardness.getName() ||
       propertyName == m_rasThickness.getName()) {
@@ -1994,8 +2011,8 @@ void ToonzRasterBrushTool::loadPreset() {
     m_drawOrder.setIndex(preset.m_drawOrder);
     m_pencil.setValue(preset.m_pencil);
     m_pressure.setValue(preset.m_pressure);
+    m_assistants.setValue(preset.m_assistants);
     m_modifierSize.setValue(preset.m_modifierSize);
-
   } catch (...) {
   }
 }
@@ -2014,6 +2031,7 @@ void ToonzRasterBrushTool::addPreset(QString name) {
   preset.m_drawOrder    = m_drawOrder.getIndex();
   preset.m_pencil       = m_pencil.getValue();
   preset.m_pressure     = m_pressure.getValue();
+  preset.m_assistants  = m_assistants.getValue();
   preset.m_modifierSize = m_modifierSize.getValue();
 
   // Pass the preset to the manager
