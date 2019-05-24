ec1f94ae9699710b64720a4f2672d22938efd5bc
manongjohn <dancel@ptd.net>
2019-05-23 23:13:32 -0400

    Fix dmg creation

--- ci-scripts/osx/travis-buildpkg.sh
+++ ci-scripts/osx/travis-buildpkg.sh
@@ -4,7 +4,7 @@ echo ">>> Creating folder: $TOONZDIR/OTX/OpenToonzPortable"
 mkdir -p $TOONZDIR/OTX/OpenToonzPortable folder
 echo ">>> Copying $TOONZDIR/OpenToonz.app to $TOONZDIR/OTX/OpenToonzPortable"
 cp -r $TOONZDIR/OpenToonz.app $TOONZDIR/OTX/OpenToonzPortable 
-echo ">>> Copying thirdparty/stuff $TOONZDIR/OTX/OpenToonzPortable/portablestuff"
-cp -r thirdparty/stuff $TOONZDIR/OTX/OpenToonzPortable/portablestuff
+echo ">>> Copying stuff $TOONZDIR/OTX/OpenToonzPortable/portablestuff"
+cp -r stuff $TOONZDIR/OTX/OpenToonzPortable/portablestuff
 echo ">>> Creating $TOONZDIR/OpenToonz-OTX-osx.dmg"
 hdiutil create $TOONZDIR/OpenToonz-OTX-osx.dmg -ov -volname OpenToonzPortable -fs HFS+ -srcfolder $TOONZDIR/OTX -format UDZO
