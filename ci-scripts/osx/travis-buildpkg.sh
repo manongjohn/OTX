f34b4e38116b16755ca8d53c7874b328dd717aa3
manongjohn <dancel@ptd.net>
2019-05-23 22:36:29 -0400

    Fix scripts

--- ci-scripts/osx/travis-buildpkg.sh
+++ ci-scripts/osx/travis-buildpkg.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export TOONZDIR=toonz/build/toonz
+echo ">>> Creating folder: $TOONZDIR/OTX/OpenToonzPortable"
 mkdir -p $TOONZDIR/OTX/OpenToonzPortable folder
+echo ">>> Copying $TOONZDIR/OpenToonz.app to $TOONZDIR/OTX/OpenToonzPortable"
 cp -r $TOONZDIR/OpenToonz.app $TOONZDIR/OTX/OpenToonzPortable 
-cp -r thirdparty/stuff to $TOONZDIR/OTX/OpenToonzPortable/portablestuff
+echo ">>> Copying thirdparty/stuff $TOONZDIR/OTX/OpenToonzPortable/portablestuff"
+cp -r thirdparty/stuff $TOONZDIR/OTX/OpenToonzPortable/portablestuff
+echo ">>> Creating $TOONZDIR/OpenToonz-OTX-osx.dmg"
 hdiutil create $TOONZDIR/OpenToonz-OTX-osx.dmg -ov -volname OpenToonzPortable -fs HFS+ -srcfolder $TOONZDIR/OTX -format UDZO
