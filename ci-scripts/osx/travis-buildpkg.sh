bbde5d04095d81d9b3e479ee8f04f82669dd7eeb
manongjohn <dancel@ptd.net>
2019-05-23 21:03:20 -0400

    Testing another fix

--- ci-scripts/osx/travis-buildpkg.sh
+++ ci-scripts/osx/travis-buildpkg.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # We assume we're in opentoonz/toonz/sources/toonz
 export TOONZDIR=toonz/sources/toonz
+echo "====="
+pwd
+echo "====="
 mkdir -p $TOONZDIR/OTX/OpenToonzPortable folder
 cp -r $TOONZDIR/OpenToonz.app $T9ONZDIR/OTX/OpenToonzPortable 
 cp -r thirdparty/stuff to $TOONZDIR/OTX/OpenToonzPortable/portablestuff
-hdiutil create $TOONZDIR/OpenToonz-OTX-osx.dmg -ov -volname OpenToonzPortable -fs HFS+ -scrfolder $TOONZ/OTX -format UDZO
+hdiutil create $TOONZDIR/OpenToonz-OTX-osx.dmg -ov -volname OpenToonzPortable -fs HFS+ -srcfolder $TOONZ/OTX -format UDZO
