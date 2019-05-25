9f8ea16dc01a4eb52d7be73ac82ba8c352ee421a
manongjohn <dancel@ptd.net>
2019-05-25 11:47:29 -0400

    Change location of portablestuff folder

--- ci-scripts/osx/travis-buildpkg.sh
+++ ci-scripts/osx/travis-buildpkg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export TOONZDIR=toonz/build/toonz
-echo ">>> Creating folder: $TOONZDIR/OTX/OpenToonzPortable"
-mkdir -p $TOONZDIR/OTX/OpenToonzPortable folder
-echo ">>> Copying $TOONZDIR/OpenToonz.app to $TOONZDIR/OTX/OpenToonzPortable"
-cp -r $TOONZDIR/OpenToonz.app $TOONZDIR/OTX/OpenToonzPortable 
-echo ">>> Copying stuff $TOONZDIR/OTX/OpenToonzPortable/portablestuff"
-cp -r stuff $TOONZDIR/OTX/OpenToonzPortable/portablestuff
+echo ">>> Creating folder: $TOONZDIR/OTX"
+mkdir $TOONZDIR/OTX folder
+echo ">>> Copying $TOONZDIR/OpenToonz.app to $TOONZDIR/OTX"
+cp -r $TOONZDIR/OpenToonz.app $TOONZDIR/OTX 
+echo ">>> Copying stuff $TOONZDIR/OTX/OpenToonz.app/portablestuff"
+cp -r stuff $TOONZDIR/OTX/OpenToonz.app/portablestuff
 echo ">>> Creating $TOONZDIR/OpenToonz-OTX-osx.dmg"
 hdiutil create $TOONZDIR/OpenToonz-OTX-osx.dmg -ov -volname OpenToonzPortable -fs HFS+ -srcfolder $TOONZDIR/OTX -format UDZO
