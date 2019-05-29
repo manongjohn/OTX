918f707ced6c43a51bff5f2184fa59ce5097b87a
John Dancel <dancel@ptd.net>
2019-05-29 13:21:52 -0400

    Fix macOS portable

--- ci-scripts/osx/travis-buildpkg.sh
+++ ci-scripts/osx/travis-buildpkg.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+export QTDIR=/usr/local/opt/qt
 export TOONZDIR=toonz/build/toonz
-echo ">>> Creating folder: $TOONZDIR/OTX"
-mkdir $TOONZDIR/OTX folder
-echo ">>> Copying $TOONZDIR/OpenToonz.app to $TOONZDIR/OTX"
-cp -r $TOONZDIR/OpenToonz.app $TOONZDIR/OTX 
-echo ">>> Copying stuff $TOONZDIR/OTX/OpenToonz.app/portablestuff"
-cp -r stuff $TOONZDIR/OTX/OpenToonz.app/portablestuff
-echo ">>> Creating $TOONZDIR/OpenToonz-OTX-osx.dmg"
-hdiutil create $TOONZDIR/OpenToonz-OTX-osx.dmg -ov -volname OpenToonzPortable -fs HFS+ -srcfolder $TOONZDIR/OTX -format UDZO
+
+echo ">>> Copying stuff $TOONZDIR/OpenToonz.app/portablestuff"
+cp -R stuff $TOONZDIR/OpenToonz.app/portablestuff
+
+echo ">>> Creating OpenToonz-OTX-osx.dmg"
+
+$QTDIR/bin/macdeployqt $TOONZDIR/OpenToonz.app -dmg -verbose=0 -always-overwrite \
+   -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/lzocompress \
+   -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/lzodecompress \
+   -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/tcleanup \
+   -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/tcomposer \
+   -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/tconverter \
+   -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/tfarmcontroller \
+   -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/tfarmserver 
+   
+mv $TOONZDIR/OpenToonz.dmg $TOONZDIR/OpenToonz-OTX-osx.dmg
