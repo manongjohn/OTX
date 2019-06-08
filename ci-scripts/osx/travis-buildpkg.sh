c6a8179e4def3792be2b1160c14ab3e30e5f8314
John Dancel <dancel@ptd.net>
2019-06-07 23:28:45 -0400

    Travis build updates including Linux AppImage creation

--- ci-scripts/osx/travis-buildpkg.sh
+++ ci-scripts/osx/travis-buildpkg.sh
@@ -2,7 +2,7 @@
 export QTDIR=/usr/local/opt/qt
 export TOONZDIR=toonz/build/toonz
 
-echo ">>> Copying stuff $TOONZDIR/OpenToonz.app/portablestuff"
+echo ">>> Copying stuff to $TOONZDIR/OpenToonz.app/portablestuff"
 cp -R stuff $TOONZDIR/OpenToonz.app/portablestuff
 
 echo ">>> Creating OpenToonz-OTX-osx.dmg"
@@ -16,4 +16,4 @@ $QTDIR/bin/macdeployqt $TOONZDIR/OpenToonz.app -dmg -verbose=0 -always-overwrite
    -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/tfarmcontroller \
    -executable=$TOONZDIR/OpenToonz.app/Contents/MacOS/tfarmserver 
    
-mv $TOONZDIR/OpenToonz.dmg $TOONZDIR/OpenToonz-OTX-osx.dmg
+mv $TOONZDIR/OpenToonz.dmg $TOONZDIR/../OpenToonz-OTX-osx.dmg
