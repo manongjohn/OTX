08a10214b08873ba434434afbf90c56801694f5d
manongjohn <dancel@ptd.net>
2020-09-11 02:07:11 -0400

    OTX: Restore original OT Travis scripts

--- ci-scripts/osx/travis-build.sh
+++ ci-scripts/osx/travis-build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 pushd thirdparty/tiff-4.0.3
-./configure --disable-lzma --without-x && make
+./configure --disable-lzma && make
 popd
 cd toonz && mkdir build && cd build
 QTVERSION=`ls /usr/local/Cellar/qt`
 echo "QT Version detected: $QTVERSION"
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/jpeg-turbo/lib/pkgconfig"
 cmake ../sources \
-      -DQT_PATH=/usr/local/opt/qt/lib/ \
+      -DQT_PATH=/usr/local/Cellar/qt/$QTVERSION/lib/ \
       -DTIFF_INCLUDE_DIR=../../thirdparty/tiff-4.0.3/libtiff/ \
       -DSUPERLU_INCLUDE_DIR=../../thirdparty/superlu/SuperLU_4.1/include/
 make -j 2
