86aa7eff2c5257a4cae74229498d97887cbcd111
shun-iwasawa <shun-iwasawa@users.noreply.github.com>
2021-03-17 07:50:26 +0900

    fix mac tcomposer (#3767)

--- toonz/sources/tcomposer/tcomposer.cpp
+++ toonz/sources/tcomposer/tcomposer.cpp
@@ -695,6 +695,7 @@ int main(int argc, char *argv[]) {
   // questo definisce la registry root e inizializza TEnv
   TEnv::setRootVarName(rootVarName);
   TEnv::setSystemVarPrefix(systemVarPrefix);
+  TEnv::setApplicationFileName(argv[0]);
 
   QCoreApplication::setOrganizationName("OpenToonz");
   QCoreApplication::setOrganizationDomain("");
